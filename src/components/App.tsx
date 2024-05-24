e793cdb26b9e42d9e294cdb18dfc9994e13387cf
Frederic Brodbeck <der.froedi@gmail.com>
2024-05-24 17:01:33 +0200

    refactor + initial sync

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, ReactNode, useEffect, useState } from 'react';
 import * as sdk from 'matrix-js-sdk';
-import { MatrixClient, MatrixError } from 'matrix-js-sdk';
+import { ClientEvent, MatrixClient, MatrixError } from 'matrix-js-sdk';
 
 import Login from './Login';
 import Main from './Main';
@@ -13,10 +13,19 @@ function initClient(baseUrl: string) {
 }
 
 
+type Status = 'logged-out' | 'logged-in' | 'initial-sync' | 'ready';
+
+
 function App() {
 	const [client, setClient] = useState<MatrixClient | null>(null);
 	const [user, setUser] = useState<User | null>(null);
 	const [loginErrors, setLoginErrors] = useState<string[]>([]);
+	const [status, setStatus] = useState<Status>('logged-out');
+
+	useEffect(
+		() => { console.log('status: ' + status); },
+		[status]
+	);
 
 	useEffect(
 		() => {
@@ -37,15 +46,25 @@ function App() {
 			const res = await client.loginWithPassword(user, password);
 			const { access_token, user_id } = res;
 			client.setAccessToken(access_token);
+			setStatus('logged-in');
 
-			client.startClient();
+			client.once(ClientEvent.Sync, async (state: string/* , prevState, res */) => {
+				if (state === 'PREPARED') {
+					console.log('Sync complete');
 
-			const { displayname } = await client.getProfileInfo(user_id);
+					const { displayname } = await client.getProfileInfo(user_id);
+					setUser({
+						userId: user_id,
+						displayName: displayname
+					});
 
-			setUser({
-				userId: user_id,
-				displayName: displayname
+					setStatus('ready');
+				} else {
+					console.error('Sync failed!');
+				}
 			});
+			setStatus('initial-sync');
+			client.startClient();
 		} catch (err) {
 			if (err instanceof MatrixError) {
 				console.error('Login failed:', err.data.error);
@@ -55,20 +74,30 @@ function App() {
 		}
 	};
 
+	let content: ReactNode = null;
+	if (!client) {
+		// content = 'Initializing ...';
+	} else {
+		if (status === 'logged-out') {
+			content = <Login
+				onSubmit={onLoginSubmit}
+				errors={loginErrors}
+			/>;
+		} else if (status === 'initial-sync') {
+			content = 'Syncing...'; // TODO: show spinner
+		} else if (status === 'ready') {
+			console.assert(user != null);
+			if (user === null) {
+				console.error('Invalid state');
+				return;
+			}
+			content = <Main user={user} />;
+		}
+	}
+
 	return <Fragment>
-		<h1>email notification system: frontend</h1>
-		<div>{
-			(!client)
-				? 'Initializing ...'
-				: (!user)
-					? <Login
-						onSubmit={onLoginSubmit}
-						errors={loginErrors}
-					/>
-					: <Main
-						user={user}
-					/>
-		}</div>
+		<h1>email notification system</h1>
+		<main>{content}</main>
 	</Fragment>;
 }
 
