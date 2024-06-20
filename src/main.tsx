1e4fb5b10fc0263140f555b2ab65ce026b1148d1
Andi Rueckel <andirueckel@users.noreply.github.com>
2024-06-20 18:02:13 +0200

    chore: rename env variables in .env.example

--- src/main.tsx
+++ src/main.tsx
@@ -10,14 +10,14 @@ import './index.css';
 console.log('CONFIGURATION --------');
 console.log(`App name: ${import.meta.env.VITE_APP_NAME}`);
 console.log(`Page title: ${import.meta.env.VITE_HTML_TITLE}`);
-console.log(`Matrix server: ${import.meta.env.VITE_MATRIX_SERVER_URL}`);
-console.log(`Max room age: ${import.meta.env.VITE_MAX_ROOM_AGE_DAYS}`);
+console.log(`Matrix server: ${import.meta.env.MATRIX_BASE_URL}`);
+console.log(`Max room age: ${import.meta.env.MAX_ROOM_AGE_DAYS}`);
 console.log('----------------------');
 
 document.title = import.meta.env.VITE_HTML_TITLE;
 
 const client = sdk.createClient({
-	baseUrl: import.meta.env.VITE_MATRIX_SERVER_URL
+	baseUrl: import.meta.env.MATRIX_BASE_URL
 });
 
 initI18n();
